9b0c0ba968a1268eeb9d3764b07892cf8adb5a5c
lingalvy <lingalvy@mail.uc.edu>
2016-12-03 16:37:14 -0800

    commit

--- project.py
+++ project.py
@@ -9,17 +9,26 @@ import json
 app = Flask(__name__)
 
 @app.route('/search/<string:title>',methods=['GET','POST'])
-def work(title) :
+def search(title) :
 	con = mysql.connector.connect(user='root',password='Venkat@123',host='127.0.0.1',database='project02')
 	cur = con.cursor()
-	arg = (title)
 	query = ("SELECT Details FROM cat_work WHERE Details  LIKE %s")
 	cur.execute(query,("%" + title + "%",))
 	for  Details in cur:
 		return Details
 	cur.close()
 	con.close()
-	
+@app.route('/work/<string:id>',methods=['GET','POST'])
+def work(id) :
+        con = mysql.connector.connect(user='root',password='Venkat@123',host='127.0.0.1',database='project02')
+        cur = con.cursor()
+        query = ("SELECT Details FROM cat_work WHERE Object_Id = %s")
+        cur.execute(query,(id,))
+        for  Details in cur:
+                return Details
+        cur.close()
+        con.close()
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="80")
 
