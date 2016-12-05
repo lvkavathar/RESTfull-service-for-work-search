6be759ab390da38e9976e964e8eb76da584ab628
root <root@ip-172-31-42-127.us-west-2.compute.internal>
2016-12-05 15:21:35 +0000

    changes updated

--- project.py
+++ project.py
@@ -11,29 +11,48 @@ app = Flask(__name__)
 
 @app.route('/search/<string:title>',methods=['GET','POST'])
 def search(title) :
-	con = mysql.connector.connect(user='root',password='Venkat@123',host='127.0.0.1',database='project02')
+	con = mysql.connector.connect(user='root',password='toor',host='127.0.0.1',database='project02')
 	cur = con.cursor()
-	query = ("SELECT Details FROM cat_work WHERE Details  LIKE %s")
+	query = ("SELECT Details FROM cat WHERE title  LIKE %s")
 	cur.execute(query,("%" + title + "%",))
+	try:
+	 os.remove("newfile1.txt")
+	except OSError:
+	 pass
+	
+	file = open("newfile1.txt", "w")
 	for  Details in cur:
-		return Details
-	cur.close()
-	con.close()
+                file.write(Details[0].encode('utf8'))
+		print Details[0]
+	file.close()
+        cur.close()
+        con.close()        
+	file = open("newfile1.txt", "r")
+        return file.read().decode('utf8')	
+	
 @app.route('/work/<string:id>',methods=['GET','POST'])
 def work(id) :
-        con = mysql.connector.connect(user='root',password='Venkat@123',host='127.0.0.1',database='project02')
+        con = mysql.connector.connect(user='root',password='toor',host='127.0.0.1',database='project02')
         cur = con.cursor()
-        query = ("SELECT Details FROM cat_work WHERE Object_Id = %s")
+        query = ("SELECT Details FROM cat WHERE Object_Id = %s")
         cur.execute(query,(id,))
+	try:
+         os.remove("newfile.txt")
+        except OSError:
+         pass
+	file = open("newfile.txt", "w")
         for Details in cur:
 		data = json.loads(Details[0])
 		url = "https://openlibrary.org"+data["authors"][0]["author"]["key"] +".json"
   		response=requests.get(url)
   		result = json.loads(response.content)
-   		return  result["name"]
-
+		file.write(Details[0].encode('utf8'))
+		file.write(result["name"].encode('utf8'))
+		file.close()
         cur.close()
         con.close()
+	file = open("newfile.txt", "r")
+        return file.read().decode('utf8')
 
 
 if __name__ == '__main__':
--- storedata.py
+++ storedata.py
@@ -2,15 +2,18 @@
 
 import mysql.connector
 import sys
+import json
 for line in sys.stdin:
 	line = line.strip()
 	words=line.split('\t')
 	words[1]=words[1].replace("/works/","")
 	words[2] = int(words[2])
-	con = mysql.connector.connect(user='root',password='Venkat@123',host='127.0.0.1',database='project02')
+	data = json.loads(words[04])
+	title = data["title"]
+	con = mysql.connector.connect(user='root',password='toor',host='127.0.0.1',database='project02')
 	cur = con.cursor()
-	arg = (words[1],words[2],words[3],words[4])
-	query = ("INSERT INTO cat_work values (%s,%s,%s,%s)")
+	arg = (words[1],words[2],words[3],words[4],title)
+	query = ("INSERT INTO cat values (%s,%s,%s,%s,%s)")
 	cur.execute(query, arg)
 	con.commit()
 	cur.close()
